5289dcd95091dd30cb547bd8ae77c26bb3b5f7aa
synesis1 <dyngafiodor@gmail.com>
2024-12-23 15:52:04 +0300

    second

--- homework/task1/models.py
+++ homework/task1/models.py
@@ -6,14 +6,14 @@ from django.db.models import ForeignKey
 
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField
-    age = models.IntegerField
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    age = models.IntegerField(default=18)
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField
-    size = models.DecimalField
-    description = models.TextField
+    cost = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    size = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    description = models.TextField(default='nothing')
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
