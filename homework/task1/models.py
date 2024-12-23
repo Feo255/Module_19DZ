1ea95e737ccfa1bcb63e4bc22f231cf92723328d
synesis1 <dyngafiodor@gmail.com>
2024-12-23 12:34:21 +0300

    first commit

---
+++ homework/task1/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models import ForeignKey
+
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField
+    age = models.IntegerField
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField
+    size = models.DecimalField
+    description = models.TextField
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='name')
+
