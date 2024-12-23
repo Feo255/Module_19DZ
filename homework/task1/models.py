987db543e803298810001e22ff99b2e65c71849f
synesis1 <dyngafiodor@gmail.com>
2024-12-23 12:34:21 +0300

    first commit

--- homework/task1/models.py
+++ homework/task1/models.py
@@ -15,5 +15,4 @@ class Game(models.Model):
     size = models.DecimalField
     description = models.TextField
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='name')
 
